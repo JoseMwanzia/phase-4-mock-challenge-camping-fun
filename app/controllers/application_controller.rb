814fec3271d7c803972730144796b4a82ecc84d4
joseph mwanzia <makamwanzia@gmail.com>
2023-05-17 13:30:00 +0300

    completed labs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+
+
+
+  def render_not_found_response(e)
+      render json: {
+          error: e.to_s
+        }, 
+        status: :not_found
+  end
+
+  def render_invalid_response(invalid)
+    render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
 
 end
